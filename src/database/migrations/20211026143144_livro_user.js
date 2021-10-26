f7782b5c3fdc5aead7e47b232449666e98c65bc1
Julia Tarchi Maia de Souza <jutarchi00@gmail.com>
2021-10-26 19:17:36 -0300

    Arrumei table livro + user

--- src/database/migrations/20211026143144_livro_user.js
+++ src/database/migrations/20211026143144_livro_user.js
@@ -1,12 +1,18 @@
 exports.up = function(knex) {
     return knex.schema.createTable('livro_user', function(table){
-        table.string('book_id').primary().notNullable();
-        table.foreign("book_id").references("book_id").inTable("livro");
-        table.string('user_id').primary().notNullable();
-        table.foreign("user_id").references("user_id").inTable("user").onDelete("cascade");
-        table.string('título').primary().notNullable();
-        table.string('autor').primary().notNullable();
+        table.string('book_id').notNullable();
+        table
+            .foreign("book_id")
+            .references("book_id")
+            .inTable("livro")
+            .onDelete("cascade");
 
+        table.string('user_id').primary().notNullable();
+        table
+            .foreign("user_id")
+            .references("user_id")
+            .inTable("user")
+            .onDelete("cascade");
      });
   
 };
